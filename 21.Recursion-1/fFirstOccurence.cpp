ba83b788419a814de12b3ccba01bae8feae2e8e4
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2025-08-05 22:37:06 +0530

    Add files via upload

--- 21.Recursion-1/fFirstOccurence.cpp
+++ 21.Recursion-1/fFirstOccurence.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int firstOccur(v, int n, int target){
-    if(i == n) return -1;
-    if(arr[i] == target) return i;
-    return firstOccur(arr, i+1)
-}
-int main(){
-    vector<int>arr = {1,2,3,3,3,4};
 
-    cout<<firstOccur(arr, target);
+int firstOccur(vector<int> arr, int i, int target) {
+    if(i == arr.size()) return -1;         // Base case: target not found
+    if(arr[i] == target) return i;         // Base case: target found
+    return firstOccur(arr, i + 1, target); // Recursive call
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 3, 3, 4};
+    cout << firstOccur(arr, 0, 3); 
     return 0;
 }
